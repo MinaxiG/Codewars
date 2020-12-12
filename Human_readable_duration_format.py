1c373256b1427aa57153ea492958ca0faa01b412
Minaxi Goel <44741447+MinaxiG@users.noreply.github.com>
2020-12-12 19:07:25 +0530

    Create Human_readable_duration_format.py

---
+++ Human_readable_duration_format.py
@@ -0,0 +1,24 @@
+def format_duration(seconds):
+    #your code here
+    s = seconds
+    y = seconds//60//60//24//365                      # no. of years
+    d = (s-y*60*60*24*365)//60//60//24                # no. of days
+    h = (s-y*60*60*24*365-d*60*60*24)//60//60         # no. of hours    
+    m = (s-y*60*60*24*365-d*60*60*24-h*3600)//60      # no. of minutes
+    ss = (s-y*60*60*24*365-d*60*60*24-h*3600-m*60)    # no. of seconds
+    
+    a = [y, d, h, m, ss]
+    s = ['year', 'day', 'hour', 'minute', 'second']
+    p = ['years', 'days', 'hours', 'minutes', 'seconds']
+    
+    r = [idx for idx,i in enumerate(a) if i>=1]
+    res = ["{}".format(str(a[idx]))+' '+p[idx] if a[idx]>1 else "{}".format(str(a[idx]))+' '+s[idx] for idx in r]
+    if len(res) == 0:
+        return 'now'    
+    if len(res)==1:
+        return '{}'.format(''.join(res))
+    else:
+        l = len(res)
+        res[l-2] = res[l-2]+' and '+res[l-1]
+        res.pop()
+        return '{}'.format(', '.join(res))
